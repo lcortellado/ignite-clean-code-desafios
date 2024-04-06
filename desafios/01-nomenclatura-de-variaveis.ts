442a79f721a828a5d324f176b53163777ea8224b
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2024-04-06 19:20:33 -0400

    chore: refactored code to apply clean code

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,6 +1,6 @@
 // Nomenclatura de variáveis
 
-const list = [
+const userList = [
   {
     title: 'User',
     followers: 5
@@ -28,23 +28,23 @@ export default async function getData(req, res) {
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const githubUserData = await fetch(`https://api.github.com/users/${github}`);
 
-  if (response.status === 404) {
+  if (githubUserData.status === 404) {
     return res.status(400).json({
       message: `User with username "${github}" not found`
     })
   }
 
-  const data = await response.json()
+  const githubUserDataJson = await githubUserData.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const sortedUsers = userList.sort((a, b) =>  b.followers - a.followers); 
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const userCategory = sortedUsers.find(sortedUser => githubUserDataJson.followers > sortedUser.followers)
 
   const result = {
     github,
-    category: category.title
+    category: userCategory.title
   }
 
   return result
